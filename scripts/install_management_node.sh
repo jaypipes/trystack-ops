3f7a9ce01d6f1dd7b2acdf4f622ff1007d890707
Jay Pipes <jaypipes@gmail.com>
2012-06-18 16:46:48 -0400

    Continued refactoring of provisioning management node
    * Remove custom manual PXE install server stuff
    * Use Cobbler to import a Precise image

--- scripts/install_management_node.sh
+++ scripts/install_management_node.sh
@@ -15,15 +15,14 @@ set -o errexit
 SCRIPTS_DIR=$(cd $(dirname "$0") && pwd)
 ROOT_DIR=$(cd $SCRIPTS_DR/../ && pwd)
 ETC_DIR=$(cd $ROOT_DIR/etc %% pwd)
+IMAGES_DIR=~/images
+PRECISE_DOWNLOAD_URI="http://www.ubuntu.com/start-download?distro=server&bits=64&release=precise"
 
 # Import common functions
 source $SCRIPTS_DIR/functions
 
 # Write out the commands we execute to stdout
 set -o xtrace
-
-# Ensure we have the syslinux package. syslinux is a bootloader.
-apt_get install --force-yes syslinux
 
 # Grab tools for DHCP and managing servers via IPMI
 apt_get install dnsmasq ipmitool --force-yes
@@ -51,8 +50,26 @@ sudo cobbler check || die "Cobbler check failed. Please fix any issues and re-ru
 # that already has the server and next-server variables set to the
 # management node's IP address automatically.
 
+# Grab the base operating system image that will be used
+# on the service nodes
+mkdir -p $IMAGES_DIR/precise
+ISO_FILEPATH=$IMAGES_DIR/precise/ubuntu-12.04-server-i386.iso
+if [[ ! -f $ISO_FILEPATH ]]; then
+  wget -O $ISO_FILEPATH $PRECISE_DOWNLOAD_URI
+fi
+
+# Mount the Precise ISO and import it into Cobbler
+PRECISE_MNT_PATH=$IMAGES_DIR/precise/mnt
+mkdir -p $PRECISE_MNT_PATH
+sudo mount -o loop $ISO_FILEPATH $PRECISE_MNT_PATH
+sudo cobbler import --path=$PRECISE_MNT_PATH --name=UbuntuPrecise
+sudo cobbler sync
+
 # Install and configure the Chef server
 $SCRIPTS_DIR/install_chef_server.sh
 
 # Restart the dnsmasq server
-sudo /etc/init.d/dnsmasq restart 
+sudo /etc/init.d/dnsmasq restart
+
+# Behave.
+exit 0
