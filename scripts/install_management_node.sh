3ef82fd90e8056d665b3ee4a693ccd54f5e1b670
Jay Pipes <jaypipes@gmail.com>
2012-06-20 15:38:05 -0400

    Adds node info file and var dir
    * Adds a data directory to store info files
    * Call cobbler system add for all service nodes

--- scripts/install_management_node.sh
+++ scripts/install_management_node.sh
@@ -9,14 +9,25 @@ if [ $USER -ne "root" ]; then
   exit 1
 fi
 
+if [ -z $ZONE_ID -a $# -eq 0 ]; then
+  print "You need to set the ZONE_ID environment variable or "
+  print "supply it as the first argument to this script."
+fi
+
 # Exit on error to stop unexpected errors
 set -o errexit
 
 SCRIPTS_DIR=$(cd $(dirname "$0") && pwd)
 ROOT_DIR=$(cd $SCRIPTS_DR/../ && pwd)
-ETC_DIR=$(cd $ROOT_DIR/etc %% pwd)
-IMAGES_DIR=~/images
+VAR_DIR=$(cd $ROOT_DIR/var && pwd)
+ETC_DIR=$(cd $ROOT_DIR/etc && pwd)
 PRECISE_DOWNLOAD_URI="http://www.ubuntu.com/start-download?distro=server&bits=64&release=precise"
+
+# Create the images download and mount directories
+IMAGES_DIR=$ROOT_DIR/images
+if [[ ! -d $IMAGES_DIR ]]; then
+  mkdir -p $IMAGES_DIR/precise/mnt
+fi
 
 # Import common functions
 source $SCRIPTS_DIR/functions
@@ -52,9 +63,8 @@ sudo cobbler check || die "Cobbler check failed. Please fix any issues and re-ru
 
 # Grab the base operating system image that will be used
 # on the service nodes
-mkdir -p $IMAGES_DIR/precise
-ISO_FILEPATH=$IMAGES_DIR/precise/ubuntu-12.04-server-i386.iso
-if [[ ! -f $ISO_FILEPATH ]]; then
+ISO_FILEPATH=$IMAGES_DIR/precise/ubuntu-12.04-server-amd64.iso
+if [ ! -f $ISO_FILEPATH ]; then
   wget -O $ISO_FILEPATH $PRECISE_DOWNLOAD_URI
 fi
 
@@ -62,8 +72,34 @@ fi
 PRECISE_MNT_PATH=$IMAGES_DIR/precise/mnt
 mkdir -p $PRECISE_MNT_PATH
 sudo mount -o loop $ISO_FILEPATH $PRECISE_MNT_PATH
-sudo cobbler import --path=$PRECISE_MNT_PATH --name=UbuntuPrecise
+sudo cobbler import --path=$PRECISE_MNT_PATH --name=precise --arch=x86_64
 sudo cobbler sync
+sudo umount $PRECISE_MNT_PATH
+
+# Adds a profile to Cobbler for a base service node. Note
+# that we use Chef roles to further classify what packages
+# and other things are set up on individual service nodes
+sudo cobbler profile add --name=service_node --distro=precise-x86_64
+
+# In bash ${VAR,,} will lowercase VAR
+NODE_INFO_FILE=$VAR_DIR/zones/${ZONE_ID,,}.info
+if [ ! -f $NODE_INFO_FILE ]; then
+  die "Could not locate node info file for zone $ZONE_ID. Expected $NODE_INFO_FILE."
+fi
+
+# Create a record in Cobbler for each service node in the zone
+for line in $(<${NODE_INFO_FILE}); do
+  local hostname mac ip info
+  if [[ $line ~= ^# ]]; then
+    continue  # Skip comment lines...
+  fi
+  # Each line in the service node info file is like so:
+  # HOSTNAME MAC IP
+  hostname=$(echo $line | cut -f 1)
+  mac=$(echo $line | cut -f 2)
+  ip=$(echo $line | cut -f 3)
+  sudo cobbler system add --name=$hostname --mac=$mac --ip-address=$ip --profile=service_node --netboot-enabled=true
+done
 
 # Install and configure the Chef server
 $SCRIPTS_DIR/install_chef_server.sh
